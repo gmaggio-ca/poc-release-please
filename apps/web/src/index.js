380107716cc94dbd9e2eb55860a25136293b1094
Gianluca Maggio @Code Architects <gmaggio@codearchitects.com>
2025-09-24 12:23:22 +0200

    fix: change default port from 5043 to 5044

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { greet } from '@example/utils';
 
 const app = express();
-const port = process.env.PORT || 5043;
+const port = process.env.PORT || 5044;
 
 app.get('/', (_req, res) => {
   
